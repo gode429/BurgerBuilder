b029f6146581ee101b2f82155e0c6adbd4a25e18
gode429 <gode429@gmail.com>
2020-10-02 17:16:02 +0530

    Removed some typos and changed auth.css

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -5,7 +5,7 @@ import { Redirect } from 'react-router-dom';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import Spinner from '../../components/UI/Spinner/Spinner';
-import classes from './Auth.css';
+import classes from './Auth.module.css';
 import * as actions from '../../store/actions/index';
 
 class Auth extends Component {
